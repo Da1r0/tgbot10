f786b12db53fa93385c5811841746c400f76c7f6
Da1r0 <timdor2008@gmail.comq>
2023-07-14 14:12:34 +0700

    Initial commit

---
+++ handlers/common.py
@@ -0,0 +1,11 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+from API.waifu import waifu_api
+
+common_router = Router()
+
+@common_router.message(Command('start'))
+async def start_command(message: Message):
+    waifu = await waifu_api.get_waifu('waifu')
+    await message.answer_photo(waifu)
